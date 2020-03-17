85655aaac881e17de9da16c29c87337b575bbcdc
Zain Nadeem <zainlove@me.com>
2020-03-17 19:57:26 -0400

    Initial Commit

---
+++ Marvel-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel-Demo
+//
+//  Created by Zain Nadeem on 3/17/20.
+//  Copyright © 2020 Zain Nadeem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvel-DemoTests/Marvel_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Marvel_DemoTests.swift
+//  Marvel-DemoTests
+//
+//  Created by Zain Nadeem on 3/17/20.
+//  Copyright © 2020 Zain Nadeem. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Demo
+
+class Marvel_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
